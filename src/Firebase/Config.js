e02538be5efdd1e332c5f6fd71e48331df313aa3
MohanPrasadN <mohanprasad10n@gmail.com>
2025-01-08 15:27:15 -0500

    added files
    added files

--- src/Firebase/Config.js
+++
@@ -1,7 +0,0 @@
-
-import { getAuth } from "firebase/auth";
-
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
